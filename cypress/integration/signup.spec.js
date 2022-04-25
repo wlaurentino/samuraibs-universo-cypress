6b91209eaa14126908baf70ad1b95809b676616e
unknown <Suporte@STFSAON006140-L.stefanini.dom>
2022-04-25 19:43:55 -0300

    retirando a sub func only do contexto de campos obrigatórios para o cadastro

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -101,7 +101,7 @@ describe('cadastro', function () {
 
     })
 
-    context.only('quando não preencho nenhum dos campos', function(){
+    context('quando não preencho nenhum dos campos', function(){
         const alertMessages = [
             'Nome é obrigatório',
             'E-mail é obrigatório',
