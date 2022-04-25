510a3de8c1199663f106cb66ce9ad6b68c0db27d
unknown <Suporte@STFSAON006140-L.stefanini.dom>
2022-04-25 18:54:23 -0300

    meu primeiro commit com git

---
+++ cypress/support/components/toast/index.js
@@ -0,0 +1,14 @@
+
+import { el } from './elements'
+
+class Toast {
+    shouldHaveText(expectText) {
+        cy.get(el.toast)
+            .should('be.visible')
+            .find('p')
+            .should('have.text', expectText)
+
+    }
+}
+
+export default new Toast()
